15055dda4fb20e981566f5b299d135004b051534
Moonrise1275 <nemo1275@gmail.com>
2014-09-15 18:07:36 +0900

    add copyright comments

--- src/opencraft/event/world/EventGenerateChunk.java
+++ src/opencraft/event/world/EventGenerateChunk.java
@@ -1,3 +1,18 @@
+/*
+ * OpenCraft - Build your open world!
+ * 
+ * OpenCraft is a open source game platform to encourage minecraft style modding.
+ * All code is written by it's own author, from zero-based.
+ * This project is distributed under MIT license.
+ * 
+ * ------------------------------------------------------------------------------
+ * 
+ * class EventGenerateChunk
+ * 
+ * author - Moonrise1275
+ * All rights reserved.
+ */
+
 package opencraft.event.world;
 
 import opencraft.lib.event.IEvent;
--- src/opencraft/event/world/chunk/EventBlockChanged.java
+++ src/opencraft/event/world/chunk/EventBlockChanged.java
@@ -1,3 +1,18 @@
+/*
+ * OpenCraft - Build your open world!
+ * 
+ * OpenCraft is a open source game platform to encourage minecraft style modding.
+ * All code is written by it's own author, from zero-based.
+ * This project is distributed under MIT license.
+ * 
+ * ------------------------------------------------------------------------------
+ * 
+ * class EventBlockChanged
+ * 
+ * author - Moonrise1275
+ * All rights reserved.
+ */
+
 package opencraft.event.world.chunk;
 
 import opencraft.lib.event.IEvent;
